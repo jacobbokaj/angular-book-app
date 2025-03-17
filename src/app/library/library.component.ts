002d149d262e0d693ddcd65cbf303fa8150199ff
unknown <jacobvestergaard@hotmail.dk>
2025-03-17 12:38:17 +0100

    Can add a book to list by api on the website

--- src/app/library/library.component.ts
+++ src/app/library/library.component.ts
@@ -1,19 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {RouterModule} from '@angular/router';
-import { FormControl, FormGroup} from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { LibraryService } from '../library.service';
+import { Book } from '../ts-interfaces/book';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-library',
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule, ReactiveFormsModule,CommonModule],
   templateUrl: './library.component.html',
   styleUrl: './library.component.css'
 })
 export class LibraryComponent {
-  
+  libraryService: LibraryService = inject(LibraryService);
+  bookList: Book[] = [];
+  bookSearched?: Book;
 
   bookSearch = new FormGroup({
-    id: new FormControl(0)
+    id: new FormControl('')
   });
 
+  searchBook(){
+    if(this.bookSearch.value.id != undefined){
+
+      this.libraryService.getBookById(this.bookSearch.value.id || '').subscribe(book => {
+        if (book.data != undefined) {
+          this.bookSearched = book;
+        }else {
+          this.bookSearched = undefined;
+        }
+      });
+    }
+  }
+
+  addBook(){
+    if(this.bookSearched != undefined){
+
+      let isBookFound: boolean = false;
+      for (let index = 0; index < this.bookList.length; index++) {
+        if(this.bookList[index].data.Title == this.bookSearched.data.Title){
+          isBookFound = true;
+        }     
+      }
+
+      if(isBookFound == false){
+        this.bookList.push(this.bookSearched);
+      }
+
+      this.bookSearched = undefined;
+      this.bookSearch.patchValue({id: undefined});
 
 
+    }
+  }
+
+
+
+  deleteBook(book: Book){
+    this.bookList = this.bookList.filter(x => x !== book);
+  }
 }
